8f564fb08f748b464ff415f0a054e5408b2d5aa8
KatHe <he.katherine321@gmail.com>
2018-10-02 13:34:05 -0400

    first commit

---
+++ models/users.js
@@ -0,0 +1,48 @@
+module.exports = function (sequelize, DataTypes) {
+    var User = sequelize.define("User", {
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        googleUser: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        emailReqLink: {
+            type: DataTypes.STRING,
+        },
+        img: {
+            type: DataTypes.TEXT,
+            validate: {
+                len: [1]
+            }
+        }
+    });
+
+    User.associate = function (models) {
+        User.hasMany(models.Temps, {
+            onDelete: "cascade"
+        });
+    };
+
+    User.associate = function (models) {
+        User.hasMany(models.MailGroup, {
+            onDelete: "cascade"
+        });
+    };
+
+    return User;
+};
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,36 @@
+// NOTES: need from backend user's name, check if logged in
+var db = require("../models")
+
+module.exports = function (app) {
+
+    // load Login page or first page
+    app.get("/", function (req, res) {
+        res.render("index")
+    })
+
+    //GOOGLE INSTALLATION
+    app.get("/installation", function (req, res) {
+        res.render("installation")
+    });
+
+    // load Login page or all other pages
+
+    app.get("/:pages", function(req, res) {
+        console.log("hello");
+        
+        var pageArr = ["about", "newmail", "newtemp", "setting", "usermail", "usertemp", "", "first", "index", "preview"]
+        var pages = req.params.pages
+        if (pageArr.indexOf(pages) > -1) {
+            res.render(pages)
+        } else {
+            res.render("404")
+        }
+    })
+
+    // Render 404 page for any unmatched routes
+    app.get("*", function (req, res) {
+        res.render("404")
+    });
+
+};
+
